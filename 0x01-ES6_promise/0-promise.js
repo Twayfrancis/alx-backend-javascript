2d3ed785bfc3647df46bf9ded65d4aee9134bf6f
twayfrancis <tawaifrancis@gmail.com>
2023-11-28 21:49:34 +0300

    fix eslint

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,5 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => { });
+function getResponseFromAPI() {
+  return Promise.resolve();
 }
+
+export default getResponseFromAPI;

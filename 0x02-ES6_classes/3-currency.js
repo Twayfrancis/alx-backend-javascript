5c8433ea840d22256997f1bdd9ec53bcb30b54d5
twayfrancis <tawaifrancis@gmail.com>
2023-11-30 14:35:57 +0300

    fix eslint

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,35 +1,26 @@
-class Currency {
+export default class Currency {
   constructor(code, name) {
-    this._code = this.validateString(code, 'Code');
-    this._name = this.validateString(name, 'Name');
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(code) {
+    this._code = code;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(newCode) {
-    this._code = this.validateString(newCode, 'Code');
+  set name(name) {
+    this._name = name;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(newName) {
-    this._name = this.validateString(newName, 'Name');
-  }
-
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
-
-  validateString(value, attribute) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attribute} must be a string`);
-    }
-    return value;
-  }
 }
-
-export default Currency;

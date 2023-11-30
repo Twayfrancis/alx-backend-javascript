a9b340e60d9fdc3d2bb1d68480d7735e5351846a
twayfrancis <tawaifrancis@gmail.com>
2023-11-30 14:54:25 +0300

    fix eslint

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,26 +1,20 @@
 import Building from './5-building.js';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft); // Call the constructor of the parent class
-    this._floors = this.validateNumber(floors, 'Floors');
+    this._floors = floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  validateNumber(value, attribute) {
-    const parsedValue = parseFloat(value);
-    if (isNaN(parsedValue) || typeof parsedValue !== 'number') {
-      throw new TypeError(`${attribute} must be a number`);
-    }
-    return parsedValue;
-  }
-
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
-
-export default SkyHighBuilding;

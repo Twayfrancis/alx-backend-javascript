28d2d6f52738969076371b267d5ab7274abaa7bf
twayfrancis <tawaifrancis@gmail.com>
2023-12-04 20:11:48 +0300

    fix eslint

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,7 +5,7 @@ export default function cleanSet(set, startString) {
   let result = '';
   for (const item of set) {
     if (item.startsWith(startString)) {
-      result += item.slice(startString.length) + '-';
+      result += `${item.slice(startString.length)}-`;
     }
   }
   return result.slice(0, -1);

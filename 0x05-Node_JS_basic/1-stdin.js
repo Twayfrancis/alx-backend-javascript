688cb1499c28b3579ae0f285c6c6092512a725f6
Twayfrancis <tawaifrancis@gmail.com>
2024-01-29 23:25:15 +0300

    a program named 1-stdin.js that will be executed through command line:
    It should display the message Welcome to Holberton School, what is your name? (followed by a new line)
    The user should be able to input their name on a new line
    The program should display Your name is: INPUT
    When the user ends the program, it should display This important software is now closing (followed by a new line)

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,15 @@
-// Import the readline module
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an instance of the readline interface
-const rl = readline.createInterface({
-  input: process.stdin, // Use the standard input stream
-  output: process.stdout, // Use the standard output stream
+process.stdin.resume();
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-// Display a message and wait for user input
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // The name parameter contains the user input
-  console.log(`Your name is: ${name}`);
-  // End the program and close the readline interface
-  rl.close();
-});
-
-// Listen to the close event
-rl.on('close', () => {
-  // Display a message and exit the program
-  console.log('This important software is now closing\n');
-  process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
